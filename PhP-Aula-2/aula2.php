5fd3ff9374abc8ed4eadfe5efc2a1461e4f6d02c
Isaac <isaacvcarv4lho@gmail.com>
2025-08-15 10:49:34 -0300

    Aula 2 - php

---
+++ PhP-Aula-2/aula2.php
@@ -0,0 +1,22 @@
+<?php
+    
+    // 1. Definindo uma variável
+    $numero = 70;
+    
+    // 2. Realizando uma operação matemática
+    $resultado = $numero * 3;
+    
+    // 3. Exibindo o resultado na tela
+    echo "O triplo de " . $numero . " é " . $resultado . ".";
+    
+    //--------------------------------------------------------------//
+    echo "<br>";
+    echo "<br>";
+    
+    $numero = 180;
+    
+    $resultado = $numero / 6;
+    
+    echo "A divisão do quociente por  " . $numero . " é: " . $resultado . "."; 
+    
+    ?>
