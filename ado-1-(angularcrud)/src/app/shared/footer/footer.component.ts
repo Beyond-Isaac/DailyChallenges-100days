569c0c10ad5977bf46571614403257170a6198a0
Isaac <isaacvcarv4lho@gmail.com>
2026-03-16 11:00:22 -0300

    Add files via upload
    Essa foi uma lição usando o angular pela primeira vez. Algo interessante :D

---
+++ ado-1-(angularcrud)/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  direitos = "@Copyright 2025. Todos os direitos reservados a republica Brasileira!"
+
+}
